d82e73f98deee81135e66d6faa035732abff4c84
Mohith L A <mohithla24@gmail.com>
2026-07-09 00:01:09 +0530

    component to see speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Suspense, lazy } from 'react'
 import './index.css'
 import Nav from './components/Nav'
 import Hero from './components/Hero'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const Showcase = lazy(() => import('./components/Showcase'))
 const Features = lazy(() => import('./components/Features'))
@@ -38,8 +39,10 @@ function App() {
           <Footer />
         </Suspense>
       </div>
+      <SpeedInsights />
     </div>
   )
 }
 
 export default App
+
